2a6914b1d3fc2e135c6ccdec8ec8484d029432a9
Austin-Kindig <kindig.austin.ak@gmail.com>
2022-11-18 13:28:18 -0500

    .

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-//create 16x16 grid of divs
+//get user input for grid size
+let width = prompt("Please enter desired number of boxes for width:")
+let divSize = 960/width;
+//create square grid of divs
 const container = document.querySelector(".container");
-container.textContent = "Container";
 const div = document.createElement("div");
 div.classList.add("grid");
-let j = 16*16;
+div.setAttribute("style", `width: ${divSize}px;`)
+//div.setAttribute("style", `height: ${divSize}px;`)
+//let width = 16;
+let j = width**2;
 for (i=1; i<=j; i++) {
     let clone = div.cloneNode(true);
     clone.setAttribute('id', i);
-    clone.textContent = `div #${i}`;
     container.appendChild(clone);
-    
+};
 
-}
+//listen for mouse to hover over grid
+window.addEventListener("mouseover", e => e.target.classList.add('hover'));
+function mouseOver() {
+    div.setAttribute("style", "background-color: black;")
+};
+
