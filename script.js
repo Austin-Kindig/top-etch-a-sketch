2ff25fd0454a6c75588fe11a3d8cc55a2d4c1e3d
Austin-Kindig <kindig.austin.ak@gmail.com>
2022-12-01 16:59:46 -0500

    add button functionality for user input

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-//get user input for grid size
-let width = prompt("Please enter desired number of boxes for width:")
-let divSize = 960/width;
-//create square grid of divs
 const container = document.querySelector(".container");
 const div = document.createElement("div");
 div.classList.add("grid");
-div.setAttribute("style", `width: ${divSize}px;`)
-//div.setAttribute("style", `height: ${divSize}px;`)
-//let width = 16;
-let j = width**2;
-for (i=1; i<=j; i++) {
-    let clone = div.cloneNode(true);
-    clone.setAttribute('id', i);
-    container.appendChild(clone);
-};
+//implement button form to get user input for # of squares per side
+const body = document.querySelector("body");
+const button = document.querySelector("#canvasBtn");
+
+
+let divSize = 30;
+//get user input for grid size
+button.addEventListener("click", function() {
+    while (document.querySelector(".container").firstChild) {
+        document.querySelector(".container").removeChild(document.querySelector(".container").firstChild);
+    };
+    let width = 0;
+    while (width < 1 || width > 100) {
+        width = prompt("Please enter desired number of pixels for width(eg.1-100):")
+    };
+    divSize = 960/width;
+    //create square grid of divs
+    div.setAttribute("style", `width: ${divSize}px;`);
+    let j = width**2;
+    for (i=1; i<=j; i++) {
+        let clone = div.cloneNode(true);
+        clone.setAttribute('id', i);
+        container.appendChild(clone);
+    };
+});
 
 //listen for mouse to hover over grid
-window.addEventListener("mouseover", e => e.target.classList.add('hover'));
+container.addEventListener("mouseover", e => e.target.classList.add('hover'));
 function mouseOver() {
     div.setAttribute("style", "background-color: black;")
 };
